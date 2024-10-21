a2d0b7328b06940c60d7dc75b6390f8ede968220
Ahmed Badr <feedbackwe4u@gmail.com>
2024-10-21 20:51:44 +0300

    add helper functions

---
+++ app/Helpers/Functions.php
@@ -0,0 +1,43 @@
+<?php
+
+if (!function_exists('successResponse')) {
+    function successResponse($message = 'Success', $data = [], $statusCode = 200): \Illuminate\Http\JsonResponse
+    {
+        return response()->json([
+            'status' => true,
+            'message' => $message,
+            'data' => $data,
+        ], $statusCode);
+    }
+}
+
+if (!function_exists('errorResponse')) {
+    function errorResponse($message = 'An error occurred', $statusCode = 400, $data = []): \Illuminate\Http\JsonResponse
+    {
+        return response()->json([
+            'status' => false,
+            'message' => $message,
+            'data' => $data,
+        ], $statusCode);
+    }
+}
+
+if (!function_exists('uploadFile')) {
+    function uploadFile($file, $directory = 'uploads', $allowedExtensions = ['jpg', 'jpeg', 'png', 'pdf'])
+    {
+        if ($file && $file->isValid()) {
+            $extension = $file->getClientOriginalExtension();
+
+            // Validate the file extension
+            if (!in_array(strtolower($extension), $allowedExtensions)) {
+                return false;
+            }
+
+            $fileName = time() . '_' . $file->getClientOriginalName();
+            $path = $file->storeAs($directory, $fileName, 'public');
+            return $path;
+        }
+        return false;
+    }
+}
+
